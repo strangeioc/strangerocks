431554452ac10e31b9f4c04baf2eb77491b7d7a2
srimarc <mtanenbaum@thirdmotion.com>
2014-01-03 12:59:22 -0800

    JSON config file and ability to read it.

---
+++ StrangeRocks/Assets/scripts/strangerocks/game/model/GameConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class GameConfig
+	{
+		public GameConfig ()
+		{
+		}
+	}
+}
+
---
+++ StrangeRocks/Assets/scripts/strangerocks/game/model/IGameConfig.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public interface IGameConfig
+	{
+	}
+}
+
